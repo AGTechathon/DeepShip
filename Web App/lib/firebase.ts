88bd0a6c8335e632e5e042f0d4fde5346f45ee8e
thekarampuri <akhilkarampuri25@gmail.com>
2025-06-14 16:52:42 +0530

    Integrated Google and Email Authentication

--- Web App/lib/firebase.ts
+++ Web App/lib/firebase.ts
@@ -4,14 +4,14 @@ import { getDatabase } from "firebase/database"
 import { getAnalytics } from "firebase/analytics"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCjVngsN-2zTMhzG_L3vstc-sWuSVaajeY",
-  authDomain: "ag-hackathon.firebaseapp.com",
-  databaseURL: "https://ag-hackathon-default-rtdb.firebaseio.com", // Add this for Realtime Database
-  projectId: "ag-hackathon",
-  storageBucket: "ag-hackathon.firebasestorage.app",
-  messagingSenderId: "391075985944",
-  appId: "1:391075985944:web:a0c6034b0fb7a5876e616d",
-  measurementId: "G-GY4NSJLR9V",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 
 const app = initializeApp(firebaseConfig)
